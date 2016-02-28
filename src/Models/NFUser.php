ff27945eae427879a70e86039ee7d04e7ab141ae
hieupv <hieupv.est@gmail.com>
2016-02-28 22:00:44 +0700

    add NFUser model

--- src/Models/NFUser.php
+++ src/Models/NFUser.php
@@ -2,37 +2,49 @@
 
 namespace NFWP\Models;
 
-use Exception;
+require __DIR__ . '/../../../../../../../../wp-includes/pluggable.php';
 
-class NFUser
+use WP_User;
+
+class NFUser extends WP_User
 {
-    public function __construct($user = null)
-    {
-
-    }
 
     /**
-     * find user by meta data
+     * aggregate of where constrain
      *
-     * @param string $meta_key, string $meta_value
-     * @return array
+     * @var Array
      */
-    public static function findByMeta($meta_key, $meta_value)
-    {
-        $users = get_users(['meta_key' => $meta_key, 'meta_value' => $meta_value]);
-        return $users;
-    }
+    public $where = [];
 
     /**
-     * find user by id
+     * aggreate of all operators which can be accepted
      *
-     * @param int $id
-     * @return self object or false if no user is found
+     * @var Array
+     */
+    public $operators = ['='];
+
+    /**
+     * Retrieve user info by ID
+     *
+     * @param init $id
+     * @return self
      */
     public static function find($id)
     {
-        $user = get_user_by('id', $id);
-        return $user;
+        $user = self::get_data_by('ID', $id);
+        return new self($user);
+    }
+
+    /**
+     * Retrieve current user info
+     *
+     * @param init $id
+     * @return self
+     */
+    public static function current()
+    {
+        $user = wp_get_current_user();
+        return new self($user);
     }
 
     /**
@@ -43,59 +55,123 @@ class NFUser
      */
     public static function findOrFail($id)
     {
-        $user = get_user_by('id', $id);
+        $user = self::get_data_by('ID', $id);
         if ($user == false) {
-            throw new Exception("can not find user", 0);
-        } else {
-            return $user;
+            throw new Exception('can\'t find user', 0);
         }
+        return new self($user);
     }
 
     /**
-     * get current user
+     * set data to Where constrain
      *
-     * @return WP_User object where it can be retrieved using member variables.
+     * @param string id | ID | slug | email | login
+     * @return self
      */
-    public static function getCurrentUser()
+    public static function where($first, $second, $third = null)
     {
-        $user = wp_get_current_user();
-        if (isset($user) && $user != false) {
-            return $user;
+        $where     = self::__callStatic('getWhere');
+        $operators = self::__callStatic('getOperators');
+        $constrain = [
+            'field' => $first,
+        ];
+
+        if (in_array($second, $operators)) {
+            $constrain['operator'] = $second;
+            $constrain['value']    = $third;
         } else {
+            $constrain['operator'] = '=';
+            $constrain['value']    = $second;
+        }
+        $where[] = $constrain;
+        return self::__callStatic('setWhere', [$where]);
+    }
+
+    /**
+     * Retrieve user info
+     *
+     * @return Array
+     */
+    public function fetch()
+    {
+        $users = [];
+        foreach ($this->where as $constrain) {
+            $user = self::get_data_by($constrain['field'], $constrain['value']);
+            if ($user) {
+                $user = new self($user);
+                if (empty($users)) {
+                    array_push($users, $user);
+                } else {
+                    if ($user != $users[0]) {
+                        return [];
+                    }
+                }
+            }
+        }
+        return $users;
+    }
+
+    public function first()
+    {
+        $users = $this->fetch();
+        if (empty($users)) {
             return false;
+        } else {
+            return $users[0];
         }
     }
 
-    /**
-     * create new wordpress user and throw exception if an error is occur
-     *
-     * @param string $username, string $password, string $email
-     * @return WP_User
-     */
-    public function create($username, $password, $email, $display_name = null)
+    public static function __callStatic($method, $parameters = [])
     {
-        $username = sanitize_user($username);
-        if (isset($username) && username_exists($username)) {
-            $i = 1;
-            while (username_exists($username)) {
-                $i++;
-                $username = $username . '_' . $i;
-            }
-        }
-        if (isset($email) && email_exists($email)) {
-            $email = null;
-        }
-        $user_id = wp_create_user($username, $password, $email);
-        if (is_wp_error($user_id)) {
-            throw new Exception($user_id->get_error_message(), 0);
-        } else {
-            $user = self::find($user_id);
-            if (!isset($display_name)) {
-                $display_name = $username;
-            }
-            $user->__set('display_name', $display_name);
-            wp_update_user($user);
-            return $user;
-        }
+        $instance = new static;
+        return call_user_func_array([$instance, $method], $parameters);
+    }
+
+    /**
+     * Gets the aggregate of where constrain.
+     *
+     * @return Array
+     */
+    public function getWhere()
+    {
+        return $this->where;
+    }
+
+    /**
+     * Sets the aggregate of where constrain.
+     *
+     * @param Array $where the where
+     *
+     * @return self
+     */
+    public function setWhere(array $where)
+    {
+        $this->where = $where;
+
+        return $this;
+    }
+
+    /**
+     * Gets the aggreate of all operators which can be accepted.
+     *
+     * @return Array
+     */
+    public function getOperators()
+    {
+        return $this->operators;
+    }
+
+    /**
+     * Sets the aggreate of all operators which can be accepted.
+     *
+     * @param Array $operators the operators
+     *
+     * @return self
+     */
+    public function setOperators(array $operators)
+    {
+        $this->operators = $operators;
+
+        return $this;
     }
 }
