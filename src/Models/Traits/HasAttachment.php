d543abfaf9a96a6e4992fabc5365a3a869a8c559
Hieupv <hieupv@codersvn.com>
2016-10-08 11:28:08 +0700

    fix get default attachment

--- src/Models/Traits/HasAttachment.php
+++ src/Models/Traits/HasAttachment.php
@@ -4,22 +4,32 @@ namespace NFWP\Models\Traits;
 
 trait HasAttachment
 {
-    public $defaultImage = '';
-    
     public function thumbnail()
     {
         return $this->belongsToMany(self::class, NFWP_DB_TABLE_PREFIX . 'postmeta', 'post_id', 'meta_value')->wherePivot('meta_key', '_thumbnail_id');
     }
     public function getThumbnail()
     {
-        return $this->thumbnail->first()->guid ? $this->thumbnail->first()->guid : $this->defaultImage;
+        return $this->thumbnail->first()->guid ? $this->thumbnail->first()->guid : $this->getDefaultAttachment();
     }
     public function getThePostThumbnail($size = 'thumbnail', $attr = '')
     {
         if ($this->thumbnail->first()->guid) {
             return get_the_post_thumbnail(new WP_Post((object) $this->getAttributes()), $size, $attr);
         } else {
-            return "<img src=\"{$this->defaultImage}\" class=\"nf-default-thumbnail\"/>";
+            return "<img src=\"{$this->getDefaultAttachment()}\" class=\"nf-default-thumbnail\"/>";
+        }
+    }
+    public function getDefaultAttachment()
+    {
+        if ($this->defaultAttachment) {
+            if (is_int($this->defaultAttachment)) {
+                $this->findOrFail($this->defaultAttacment)->guid;
+            } else {
+                return $this->defaultAttacment;
+            }
+        } else {
+            return '';
         }
     }
 }
